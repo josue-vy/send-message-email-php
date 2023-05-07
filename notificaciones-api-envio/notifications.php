9b4916353463a9e4d5fa445490bc6a293422c955
josue-vy <82143374+josue-vy@users.noreply.github.com>
2023-05-06 19:01:30 -0500

    sen-message-email-php-v1

---
+++ notificaciones-api-envio/notifications.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Send Email</title>
+</head>
+<body>
+    <form class="" action="send.php" method="post">
+        Email <input type="email" name="email" value=""><br>
+        Subject <input type="text" name="subject" value=""><br>
+        Message <input type="text" name="message" value=""><br>
+        <button type="submit" name="send">send</button>
+    </form>
+</body>
+
+</html>
